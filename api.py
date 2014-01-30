5ce49256acfd8b686f60752ccc79508ddfbbf980
Anurag <anurag3rdsep@gmail.com>
2014-01-30 16:18:49 +0530

    secret

--- api.py
+++ api.py
@@ -5,7 +5,7 @@ import requests
 from flask import Flask, jsonify, request, render_template, json
 app = Flask(__name__, static_folder='static', template_folder= 'templates')
 
-app.secret_key= '\xbf\xb50\x94au\x8f\xf9\se2\x1f\x93\x06(\xdf\xe4\xaf\x1f\x86k\xb3\x2fQ%1'
+app.secret_key= 'secret'
 
 
 @app.route('/home')
